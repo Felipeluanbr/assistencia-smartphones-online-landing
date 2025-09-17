ff7395facc07d443260bda1c9031c321a19eafb4
Felipe Desenvolvedor <felipeluansilvacoelho2019@gmail.com>
2025-09-17 20:53:04 -0300

    feat: feita a criação dos objetos da lista de seletores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,128 @@
-var samsung
+var dadosOrcamento = {
+  aplle: {
+    "iPhone-XR": {
+      "tela-quebrada": 500.0,
+      "tampa-trincada": 400.0,
+      bateria: 300.0,
+      "conector-ruim": 350.0,
+    },
+
+    "iPhone-13": {
+      "tela-quebrada": 600.0,
+      "tampa-trincada": 400.0,
+      bateria: 300.0,
+      "conector-ruim": 350.0,
+    },
+
+    "iPhone-14": {
+      "tela-quebrada": 600.0,
+      "tampa-trincada": 400.0,
+      bateria: 300.0,
+      "conector-ruim": 350.0,
+    },
+
+    "iPhone-14": {
+      "tela-quebrada": 600.0,
+      "tampa-trincada": 400.0,
+      bateria: 300.0,
+      "conector-ruim": 350.0,
+    },
+  },
+
+  samsung: {
+    "S23": {
+      "tela-quebrada": 500.0,
+      "tampa-trincada": 400.0,
+      bateria: 300.0,
+      "conector-ruim": 350.0,
+    },
+
+    "S24": {
+      "tela-quebrada": 500.0,
+      "tampa-trincada": 400.0,
+      bateria: 300.0,
+      "conector-ruim": 350.0,
+    },
+
+    "S25": {
+      "tela-quebrada": 500.0,
+      "tampa-trincada": 400.0,
+      bateria: 300.0,
+      "conector-ruim": 350.0,
+    },
+  },
 
 
-var = samsung = [ "S20", "S21", "S22" , "A16", "A15", "A55"]
+  "xiomi": {
+
+    "xiomi-14":{
+        "tela-quebrada": 500.0,
+        "tampa-trincada": 400.0,
+       " bateria": 300.0,
+        "conector-ruim": 350.0,
+    
+    }, 
+
+    "Redmi":{
+        "tela-quebrada": 500.0,
+        "tampa-trincada": 400.0,
+       " bateria": 300.0,
+        "conector-ruim": 350.0,
+    
+    }, 
+
+    "Poco-X6":{
+        "tela-quebrada": 500.0,
+        "tampa-trincada": 400.0,
+       " bateria": 300.0,
+        "conector-ruim": 350.0,
+    
+    }, 
+
+    "Redmi-A3":{
+        "tela-quebrada": 500.0,
+        "tampa-trincada": 400.0,
+       " bateria": 300.0,
+        "conector-ruim": 350.0,
+    
+    }, 
+
+  },
+
+
+  "motorola" :{
+
+    "motorola-edge": {
+         "tela-quebrada": 500.0,
+        "tampa-trincada": 400.0,
+       " bateria": 300.0,
+        "conector-ruim": 350.0,
+
+    },
+
+     "moto-G84": {
+         "tela-quebrada": 500.0,
+        "tampa-trincada": 400.0,
+       " bateria": 300.0,
+        "conector-ruim": 350.0,
+
+    },
+
+     "moto-E22": {
+         "tela-quebrada": 500.0,
+        "tampa-trincada": 400.0,
+       " bateria": 300.0,
+        "conector-ruim": 350.0,
+
+    },
+
+     "motorola-razr": {
+         "tela-quebrada": 500.0,
+        "tampa-trincada": 400.0,
+       " bateria": 300.0,
+        "conector-ruim": 350.0,
+
+    },
+
+  }
+};
